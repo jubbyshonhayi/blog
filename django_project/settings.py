0a35afe0ec21965701bd46065c24357fea9020a3
jubbyshonhayi <jubilantjubby@gmail.com>
2026-03-07 14:04:54 +0530

    Remove missing users.middleware from settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -53,8 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'users.middleware.UpdateLastActivityMiddleware',
-    'users.middleware.NotificationMiddleware',
+
     
 
 ]
