5426d8b3f3430b97100e6ddcf7603893c7f53c31
jubbyshonhayi <jubilantjubby@gmail.com>
2026-03-07 15:00:12 +0530

    Editing Allowed Hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['blogging-ofno.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
